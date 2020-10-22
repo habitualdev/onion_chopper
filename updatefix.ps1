4127ab6417804192f887fce2e8c81da3e2c7921b
habitual <habitual@goldstandard-dev.habitual-media.com>
2020-10-21 20:56:40 -0400

    Removed test files

--- updatefix.ps1
+++
@@ -1,3 +0,0 @@
-﻿Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name WUServer -Value "https://windowsupdate.microsoft.com"
-Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name WUStatusServer -Value "https://windowsupdate.microsoft.com"
-Set-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate -Name DoNotConnectToWindowsUpdateInternetLocations -Value "0"
